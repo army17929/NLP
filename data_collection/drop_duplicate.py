8b0541a81194bb33a2bcb2589054c0105542b05f
army17929 <ohwang@umich.edu>
2023-06-06 15:49:48 -0400

    updated

---
+++ data_collection/drop_duplicate.py
@@ -0,0 +1,14 @@
+# Importing modules
+import pandas as pd
+import numpy as np 
+import re 
+import matplotlib.pyplot as plt 
+file =r'C:\Users\every\OneDrive\바탕 화면\Github\NLP\data_collection\data_collected(with_keywords).csv'
+
+# Above is the path of csv file. 
+df = pd.read_csv(file)
+df.columns = ['id' , 'id','tweets','keywords']
+
+# Drop duplicated values
+df = df.drop_duplicates(["tweets"],keep = "last")
+print(df.head)
