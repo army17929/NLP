9f2666065c9e2e2a03818d4a8b1939082fd91384
army17929 <ohwang@umich.edu>
2023-06-10 04:02:43 +0900

    updated

--- data_collection/drop_duplicate.py
+++ data_collection/drop_duplicate.py
@@ -3,13 +3,17 @@ import pandas as pd
 import numpy as np 
 import re 
 import matplotlib.pyplot as plt 
-file =r'C:\Users\every\OneDrive\바탕 화면\Github\NLP\data_collection\data_collected(with_keywords).csv'
+import chardet 
+
+file =r'C:\Users\every\OneDrive\바탕 화면\Github\NLP\data_collection\data_collected.csv'
+print(chardet.detect(file.encode()))
 
 # Above is the path of csv file. 
-df = pd.read_csv(file,encoding="cp1252") # The file format is cp1252
-df.columns = ['id' , 'id','tweets','keywords']
+df = pd.read_csv(file,encoding="utf-8", error_bad_lines = False) 
+df.columns = ['id_numbers','id','tweets']
+print(df.shape)
 
 # Drop duplicated values
 df = df.drop_duplicates(["tweets"], keep = "last")
-print(df.head)
+print(df.shape)
 df.to_csv('dropped_duplicates.csv')
--- twitter_streaming/twitter_streaming.py
+++ twitter_streaming/twitter_streaming.py
@@ -1,33 +1,44 @@
-# Why do we use API? 
-# First, the data is changing quickly.
-# Second, there is repeated computation involved.
-# What is API? 
 # APIs are most commonly used to retrieve data, and that will be thefocus of this beginner tutorial.
 import tweepy as tp
 import configparser
 import requests
+import pandas as pd 
+
 # Read configs
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(r"C:\Users\every\OneDrive\NLP_project\Tweeter_streaming\streaming\config.ini")
 
 api_key = config['twitter']['api_key']
 api_key_secret = config['twitter']['api_key_secret']
 
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
-print(api_key)
 
 # Authentification 
 auth = tp.OAuth1UserHandler(api_key,api_key_secret)
 auth.set_access_token(access_token,access_token_secret)
+bear = 'AAAAAAAAAAAAAAAAAAAAAApGmwEAAAAA0hwdTR1gQ63tHNb8e2uAfSuIEug%3DcY5XCQemELs7QesJ28iOzgTXbW6WTy6Mo2aNMev66bOcaDIaPh'
 
+client = tp.Client(bearer_token=bear)
 api = tp.API(auth)
+
 keyword = 'nuclear'
 count = 1 
 result = []
 while (count<=2):
-    tweets = api.search(keyword)
+    tweets = client.search_recent_tweets(keyword,max_results = 10)
     for tweet in tweets:
         result.append(tweet)
     count += 1
 print(result)
+
+import csv 
+
+with open('list_to_csv.csv','w',newline='',encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(result)
+
+original_file = r'C:\Users\every\OneDrive\바탕 화면\Github\NLP\data_collection\data_collected.csv'
+#df = pd.read_csv(original_file,encoding='utf-8',error_bad_lines = False)
+#data = df['text']
+#print(data.head())
